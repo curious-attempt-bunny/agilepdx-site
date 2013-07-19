f2045449601b8f15e6419dd46896b2f13608e3de
Paula Hannan <paula@tovabi.com>
2013-07-19 16:15:01 -0700

    Update Twitter Widget
    Twitter depricated the widget we were using.  Put in new one

--- assets/twitter-widget-config.js
+++ assets/twitter-widget-config.js
@@ -1,31 +1,45 @@
-new TWTR.Widget({
-  version: 2,
-  type: 'search',
-  search: '(agile pdx) OR agilepdx',
-  interval: 6000,
-  title: 'search: agile pdx',
-  subject: 'Agile PDX Buzz',
-  width: 250,
-  height: 300,
-  theme: {
-	shell: {
-	  background: '#39892f',
-	  color: '#ffffff'
-	},
-	tweets: {
-	  background: '#ffffff',
-	  color: '#444444',
-	  links: '#2a7322'
-	}
-  },
-  features: {
-	scrollbar: false,
-	loop: true,
-	live: true,
-	hashtags: true,
-	timestamp: true,
-	avatars: true,
-	toptweets: true,
-	behavior: 'default'
-  }
-}).render().start();
+!function(d,s,id)
+{
+  var js,
+  fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+  if(!d.getElementById(id))
+    {
+     js=d.createElement(s);
+     js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+     fjs.parentNode.insertBefore(js,fjs);
+    }
+}
+(document,"script","twitter-wjs");
+
+//  This version was depricated by twitter
+//new TWTR.Widget({
+//  version: 2,
+//  type: 'search',
+//  search: '(agile pdx) OR agilepdx',
+//  interval: 6000,
+//  title: 'search: agile pdx',
+//  subject: 'Agile PDX Buzz',
+//  width: 250,
+//  height: 300,
+//  theme: {
+//	shell: {
+//	  background: '#39892f',
+//	  color: '#ffffff'
+//	},
+//	tweets: {
+//	  background: '#ffffff',
+//	  color: '#444444',
+//	  links: '#2a7322'
+//	}
+//},
+//  features: {
+//	scrollbar: false,
+//	loop: true,
+//	live: true,
+//	hashtags: true,
+//	timestamp: true,
+//	avatars: true,
+//	toptweets: true,
+//	behavior: 'default'
+//  }
+//}).render().start();
