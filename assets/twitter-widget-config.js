48d9f7a3b055bebea49537f33157eafd26938438
Paula Hannan <paula@tovabi.com>
2013-07-23 09:20:53 -0700

    Remove Commented out Code

--- assets/twitter-widget-config.js
+++ assets/twitter-widget-config.js
@@ -10,36 +10,3 @@
     }
 }
 (document,"script","twitter-wjs");
-
-//  This version was depricated by twitter
-//new TWTR.Widget({
-//  version: 2,
-//  type: 'search',
-//  search: '(agile pdx) OR agilepdx',
-//  interval: 6000,
-//  title: 'search: agile pdx',
-//  subject: 'Agile PDX Buzz',
-//  width: 250,
-//  height: 300,
-//  theme: {
-//	shell: {
-//	  background: '#39892f',
-//	  color: '#ffffff'
-//	},
-//	tweets: {
-//	  background: '#ffffff',
-//	  color: '#444444',
-//	  links: '#2a7322'
-//	}
-//},
-//  features: {
-//	scrollbar: false,
-//	loop: true,
-//	live: true,
-//	hashtags: true,
-//	timestamp: true,
-//	avatars: true,
-//	toptweets: true,
-//	behavior: 'default'
-//  }
-//}).render().start();
